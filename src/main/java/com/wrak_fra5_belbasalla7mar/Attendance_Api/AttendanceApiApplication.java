31351d69e5dbcea2ef56d2775c6124df41e5e4bb
MohamedIbnKhaled <mohamedrobot1@gmail.com>
2025-03-17 13:48:56 +0200

    fist commit

---
+++ src/main/java/com/wrak_fra5_belbasalla7mar/Attendance_Api/AttendanceApiApplication.java
@@ -0,0 +1,13 @@
+package com.wrak_fra5_belbasalla7mar.Attendance_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AttendanceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AttendanceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wrak_fra5_belbasalla7mar/Attendance_Api/AttendanceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wrak_fra5_belbasalla7mar.Attendance_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
